0d86a0228815889bd56888d6d77d2f426f45603a
1242293215@qq.com <1242293215@qq.com>
2021-01-21 19:41:22 +0800

    feat: 翻转二叉树官方题解

--- code/src/main/java/binarytree/InvertBinaryTree.java
+++ code/src/main/java/binarytree/InvertBinaryTree.java
@@ -49,6 +49,17 @@ public class InvertBinaryTree {
         return new TreeNode(4, leftChild, rightChild);
     }
 
+    // 二叉树的翻转-官方题解
+    public TreeNode invertTree_leetcode(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+        root.left = right;
+        root.right = left;
+        return root;
+    }
     // 二叉树的翻转
     public TreeNode invertTree(TreeNode root) {
         if (root != null){
@@ -64,7 +75,6 @@ public class InvertBinaryTree {
             TreeNode temp = root.left;
             root.left = root.right;
             root.right = temp;
-
             invertPreOreder(root.left);
             invertPreOreder(root.right);
         }else{
