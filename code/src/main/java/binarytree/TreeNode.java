58921cb8a497bf540ae4e79c38bba8b9964f6609
1242293215@qq.com <1242293215@qq.com>
2021-01-21 14:39:56 +0800

    feat: 求二叉树的最大深度解答

---
+++ code/src/main/java/binarytree/TreeNode.java
@@ -0,0 +1,45 @@
+package binarytree;
+
+/**
+ * @author hawk
+ * @package binarytree
+ * @desc
+ * @date 2021/1/20
+ */
+public class TreeNode{
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode (int val){
+        this.val = val;
+    }
+    TreeNode (int val, TreeNode left, TreeNode right){
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+    TreeNode(){}
+    public int getVal() {
+        return val;
+    }
+
+    public void setVal(int val) {
+        this.val = val;
+    }
+
+    public TreeNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(TreeNode left) {
+        this.left = left;
+    }
+
+    public TreeNode getRight() {
+        return right;
+    }
+
+    public void setRight(TreeNode right) {
+        this.right = right;
+    }
+}
