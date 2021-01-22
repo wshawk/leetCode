f8c9886104c8999dddbc5996af0d25d9ea882272
文松 <ws_hawk@qq.com>
2021-01-22 14:46:32 +0800

    feat: 二叉树的第k大节点题解第一版

--- code/src/main/java/binarytree/UnivaluedBinaryTree.java
+++ code/src/main/java/binarytree/UnivaluedBinaryTree.java
@@ -54,12 +54,4 @@ public class UnivaluedBinaryTree {
                 && isUnivalTree_first(root.right)
                 && isUnivalTree_first(root.left);
     }
-    public static void preOrder(TreeNode root){
-        if (root == null){
-            return;
-        }
-        System.out.print(root.val + " ");
-        preOrder(root.left);
-        preOrder(root.right);
-    }
 }
