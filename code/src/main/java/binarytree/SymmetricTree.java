0f20a82f56c694c72c968a64a0d615a1b9a6ed41
文松 <ws_hawk@qq.com>
2021-01-23 14:45:47 +0800

    feat: 对称二叉树题解

--- code/src/main/java/binarytree/SymmetricTree.java
+++ code/src/main/java/binarytree/SymmetricTree.java
@@ -1,30 +1,30 @@
 package binarytree;
 
+import java.time.temporal.Temporal;
+
 /**
  * @author hawk
  * @package binarytree
  * @desc
  * 给定一个二叉树，检查它是否是镜像对称的。
  *
- *  
  *
- * 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+ * 例如，二叉树[1,2,2,3,4,4,3] 是对称的。
  *
  *     1
  *    / \
  *   2   2
  *  / \ / \
  * 3  4 4  3
- *  
  *
- * 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+ *
+ * 但是下面这个[1,2,2,null,3,null,3] 则不是镜像对称的:
  *
  *     1
  *    / \
  *   2   2
  *    \   \
  *    3    3
- *  
  *
  * 进阶：
  *
@@ -33,4 +33,24 @@ package binarytree;
  * @date 2021/1/22
  */
 public class SymmetricTree {
+    public static void main(String[] args) {
+
+    }
+
+    // 核心思路
+    // 1. 比较根节点下的左右孩子节点的值是否相同
+    // 2. 比较以左右孩子节点为根节点的孩子节点的 左右和右左节点的值是否相同
+    public static boolean isSymmetric(TreeNode root) {
+        return check(root, root);
+    }
+    // 递归解法
+    public static boolean check(TreeNode left, TreeNode right){
+       if (left == null && right == null){
+           return true;
+       }
+       if (left == null || right == null){
+           return false;
+       }
+       return left.val == right.val && check(left.left, right.right) && check(left.right, right.left);
+    }
 }
