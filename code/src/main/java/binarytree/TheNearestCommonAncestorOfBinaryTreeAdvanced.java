8e9446e5f93adb8923a695d5b44a01da6e772fef
文松 <ws_hawk@qq.com>
2021-01-26 14:11:13 +0800

    feat: 二叉树最近公共祖先进阶版题目初始化——网络题解

--- code/src/main/java/binarytree/TheNearestCommonAncestorOfBinaryTreeAdvanced.java
+++ code/src/main/java/binarytree/TheNearestCommonAncestorOfBinaryTreeAdvanced.java
@@ -1,9 +1,13 @@
 package binarytree;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 /**
  * @author hawk
  * @package binarytree
  * @desc 在二叉树中找到两个节点的最近公共祖先（进阶）
+ *
  * 给定一棵二叉树，多次给出这棵树上的两个节点o1和o2，请对于每次询问，找到o1和o2的最近公共祖先节点。
  *
  * 输入描述：
@@ -45,5 +49,121 @@ package binarytree;
  *
  * @date 2021/1/26
  */
+
+/*
+8 1
+1 2 3
+2 4 5
+4 0 0
+5 0 0
+3 6 7
+6 0 0
+7 8 0
+8 0 0
+4
+4 5
+5 2
+6 8
+5 8
+ */
 public class TheNearestCommonAncestorOfBinaryTreeAdvanced {
+
+    public static class Node{
+        int val;
+        Node left;
+        Node right;
+        Node parent;
+        int level;
+        public Node(int val) {
+            this.val = val;
+            left = null;
+            right = null;
+            parent = null;
+            level = -1;
+        }
+    }
+
+    public static int solution(Node[] nodes, Node node1, Node node2) {
+        while(node1 != node2) {
+            if(node1.level < node2.level) {
+                node2 = node2.parent;
+            } else if(node1.level > node2.level) {
+                node1 = node1.parent;
+            } else {
+                node1 = node1.parent;
+                node2 = node2.parent;
+            }
+        }
+        return node1.val;
+    }
+
+    public static void getLevel(Node[] nodes) {
+        for(int i = 1; i < nodes.length; i++) {
+            if(nodes[i].level != -1) {
+                continue;
+            }
+            helper(nodes[i]);
+        }
+    }
+
+    public static int helper(Node node) {
+        if(node.parent == null) {
+            return 0;
+        }
+        if(node.level != -1) {
+            return node.level;
+        }
+
+        node.level = 1 + helper(node.parent);
+
+        return node.level;
+    }
+
+
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String[] strs = bf.readLine().split(" ");
+        int n = Integer.parseInt(strs[0]);
+        int rootVal = Integer.parseInt(strs[1]);
+
+        Node[] nodes = new Node[n + 1];
+        for(int i = 0; i < n; i++) {
+            strs = bf.readLine().split(" ");
+            int f = Integer.parseInt(strs[0]);
+            int l = Integer.parseInt(strs[1]);
+            int r = Integer.parseInt(strs[2]);
+
+            if(nodes[f] == null) {
+                nodes[f] = new Node(f);
+            }
+            if(nodes[l] == null) {
+                nodes[l] = new Node(l);
+            }
+            if(nodes[r] == null) {
+                nodes[r] = new Node(r);
+            }
+
+            nodes[f].left = nodes[l];
+            nodes[f].right = nodes[r];
+
+            nodes[l].parent = nodes[f];
+            nodes[r].parent = nodes[f];
+        }
+
+        getLevel(nodes);
+
+        StringBuilder sb = new StringBuilder();
+        int q = Integer.parseInt(bf.readLine());
+        while(q-- > 0) {
+            strs = bf.readLine().split(" ");
+            int node1 = Integer.parseInt(strs[0]);
+            int node2 = Integer.parseInt(strs[1]);
+
+            sb.append(solution(nodes, nodes[node1], nodes[node2]));
+            sb.append("\n");
+        }
+        System.out.println(sb.toString().trim());
+    }
+
 }
