62cf0e57898ad42524b228d647b49a1ac7118b24
文松 <ws_hawk@qq.com>
2021-01-29 14:29:53 +0800

    feat: 面试题 02.03. 删除中间节点

---
+++ code/src/main/java/linkedlist/ListNode.java
@@ -0,0 +1,21 @@
+package linkedlist;
+
+import java.util.List;
+
+/**
+ * @author hawk
+ * @package linkedlist
+ * @desc
+ * @date 2021/1/29
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int val){
+        this.val = val;
+    }
+    ListNode(int val, ListNode next){
+        this.val = val;
+        this.next = next;
+    }
+}
