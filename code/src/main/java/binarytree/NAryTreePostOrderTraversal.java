2f5f308c1ac415e16e00ac1bf0c11ecd9b8b312a
文松 <ws_hawk@qq.com>
2021-02-19 11:44:51 +0800

    feat: 590. N叉树的后序遍历

---
+++ code/src/main/java/binarytree/NAryTreePostOrderTraversal.java
@@ -0,0 +1,28 @@
+package binarytree;
+
+import java.util.List;
+
+/**
+ * @author hawk
+ * @package binarytree
+ * @desc 590. N叉树的后序遍历
+ * 给定一个 N 叉树，返回其节点值的后序遍历。
+ *
+ * 例如，给定一个3叉树:
+ *              1
+ *            / | \
+ *           3  2  4
+ *          / \
+ *         5   6
+ *
+ * 返回其后序遍历: [5,6,3,2,4,1].
+ *
+ * 说明:递归法很简单，你可以使用迭代法完成此题吗?
+ *
+ * @date 2021/2/19
+ */
+public class NAryTreePostOrderTraversal {
+    public List<Integer> postorder(MultiTreeNode root) {
+        return null;
+    }
+}
