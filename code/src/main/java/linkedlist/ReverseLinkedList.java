e2d14226a245697fa042ac87b9fb31229d1b9142
文松 <ws_hawk@qq.com>
2021-05-19 21:07:04 +0800

    feat: 反转链表

--- code/src/main/java/linkedlist/ReverseLinkedList.java
+++ code/src/main/java/linkedlist/ReverseLinkedList.java
@@ -1,8 +1,10 @@
 package linkedlist;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
+import java.util.Stack;
 
 /**
  * @author hawk
@@ -18,16 +20,50 @@ import java.util.Queue;
  * @date 2021/2/24
  */
 public class ReverseLinkedList {
-    public ListNode reverseList(ListNode head) {
-        if (head == null) return head;
-       return null;
+    public static ListNode reverseList(ListNode head) {
+        // ListNode cur = head;
+        // ListNode pre = null;
+        // while(cur != null){
+        // 假设获取到的链表是 1 -> 2 -> 3 -> null
+        // curr 为 1 -> 2 -> 3 -> null
+        // 获取当前节点的下一个节点, 拿到 2 -> 3 -> null
+        //     ListNode next = cur.next;
+        // 1 -> null
+        //     cur.next = null;
+        //     next.next = cur;
+        //     cur = next;
+        // }
+        // return cur;
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            // 假设获取到的链表是 1 -> 2 -> 3 -> null
+            // curr 为 1 -> 2 -> 3 -> null
+            // 获取当前节点的下一个节点, 拿到 2 -> 3 -> null
+            ListNode next = curr.next;
+            // 将当前节点的下一个节点设值为当前节点的上一个节点
+            // 1 -> null
+            curr.next = prev;
+            // 将当前节点设置为下一个节点的前置节点
+            // prev = 1
+            prev = curr;
+            // curr 为 2 -> 3 -> null
+            // 此时pre链表为 1 -> null
+            curr = next;
+        }
+        return prev;
     }
 
     public static void main(String[] args) {
         ReverseLinkedList reverseLinkedList = new ReverseLinkedList();
 
         ListNode head = ListNode.generateListNode(new int[]{1, 2, 3});
+        ListNode result = reverseList(head);
+        System.out.println("ppppppppppp");
     }
-
+    public static ListNode test(ListNode head){
+        ListNode node = head;
+        return null;
+    }
 
 }
