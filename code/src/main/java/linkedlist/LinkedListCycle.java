ce3ee31d804ed19fa601b118a93fd15992461f81
文松 <ws_hawk@qq.com>
2021-09-03 15:37:50 +0800

    feat: 141. 环形链表

---
+++ code/src/main/java/linkedlist/LinkedListCycle.java
@@ -0,0 +1,27 @@
+package linkedlist;
+
+/**
+ * @author hawk
+ * @package linkedlist
+ * @desc 141. 环形链表
+ * 给定一个链表，判断链表中是否有环。
+ * 如果链表中存在环，则返回 true 。 否则，返回 false 。
+ * @date 2021/9/3
+ */
+public class LinkedListCycle {
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null){
+            return false;
+        }
+        ListNode fast = head.next.next;
+        ListNode slow = head.next;
+        while (fast != null && fast.next != null){
+            if (fast == slow){
+                return true;
+            }
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return false;
+    }
+}
