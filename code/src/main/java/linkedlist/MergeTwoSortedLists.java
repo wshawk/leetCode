819533c5a678f70ac8cd558a1f945936c1fcd349
wshawk <1242293215@qq.com>
2021-12-09 00:07:19 +0800

    feat: 21. 合并两个有序链表

---
+++ code/src/main/java/linkedlist/MergeTwoSortedLists.java
@@ -0,0 +1,59 @@
+package linkedlist;
+
+import linkedlist.node.ListNode;
+
+/**
+ * @author hawk
+ * @package linkedlist
+ * @desc 21. 合并两个有序链表
+ * 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：l1 = [1,2,4], l2 = [1,3,4]
+ * 输出：[1,1,2,3,4,4]
+ * 示例 2：
+ *
+ * 输入：l1 = [], l2 = []
+ * 输出：[]
+ * 示例 3：
+ *
+ * 输入：l1 = [], l2 = [0]
+ * 输出：[0]
+ *  
+ *
+ * 提示：
+ *
+ * 两个链表的节点数目范围是 [0, 50]
+ * -100 <= Node.val <= 100
+ * l1 和 l2 均按 非递减顺序 排列
+ *
+ * @date 2021/12/8
+ */
+public class MergeTwoSortedLists {
+    public static void main(String[] args) {
+        System.out.println(mergeTwoLists(ListNode.generateListNode(new int[]{1,3,5}),
+                ListNode.generateListNode(new int[]{2,4,6})));
+    }
+
+    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode preHead = new ListNode(-1);
+        ListNode prev = preHead;
+
+        while (l1 != null && l2 != null){
+            if (l1.val < l2.val){
+                prev.next = l1;
+                l1 = l1.next;
+            }else{
+                prev.next = l2;
+                l2 = l2.next;
+            }
+        prev = prev.next;
+        }
+        prev.next = l1 == null ? l2 : l1;
+        return preHead.next;
+    }
+}
--- code/src/main/java/linkedlist/node/ListNode.java
+++ code/src/main/java/linkedlist/node/ListNode.java
@@ -10,7 +10,7 @@ public class ListNode {
     public int val;
     public ListNode next;
     ListNode(){}
-    ListNode(int val){
+    public ListNode(int val){
         this.val = val;
     }
     ListNode(int val, ListNode next){
